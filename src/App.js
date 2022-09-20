8c128e4285204175dcf7a1a0953a50cf4965e989
Jorge Mejia <jorgemejia2304@gmail.com>
2022-09-20 16:02:41 -0500

    use connect redus

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from 'react';
+import { connect } from 'react-redux';
 
-function App() {
+import { PokemonList } from './components/PokemonList';
+import { Search } from './components/Search';
+import { getPokemon } from './api';
+
+import './App.css';
+import logo from './statics/logo.svg'
+import { Col } from 'antd';
+import { setPokemons as setPokemonsActions } from './actions';
+
+function App({pokemons, setPokemons}) {
+  useEffect(() => {
+    const fetchPokemons = async () => { 
+      const pokemonsRes = await getPokemon();
+      setPokemons(pokemonsRes); 
+    };
+    fetchPokemons();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Col span={4} offset={10}>
+        <img src={logo} alt='Pokedux' />
+      </Col>
+      <Col span={8} offset={8}>
+        <Search />
+      </Col>
+      <PokemonList pokemons={pokemons}/>
     </div>
   );
 }
 
-export default App;
+const mapStateToProps = (state) => ({
+  pokemons:state.pokemons,
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  setPokemons: (value) => dispatch(setPokemonsActions(value)),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
